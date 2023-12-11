dc7aa0219362f2dd1009fb3d8bc96386b0a555ee
thibault-chausson <chausson.thibault@laposte.net>
2023-12-11 08:32:22 +0100

    :bug:

--- mutation.py
+++ mutation.py
@@ -1,9 +1,10 @@
 import toolbox as tb
 import random as rd
 
-CHROMOSOME_1 = tb.import_population("datas\chromosome_1.json")[0]
+CHROMOSOME_1 = tb.import_population("./datas/chromosome_1.json")[0]
 
-# Swap 2 random timeslot 
+
+# Swap 2 random timeslot
 
 def swap_timeslot(chr):
     """
@@ -11,16 +12,16 @@ def swap_timeslot(chr):
     """
     # Select two random rows
     indices = rd.sample(range(len(chr)), 2)
-    
+
     # Swap values between the selected rows for "Start_Time" and "Start_day "
     temp_start_time = chr[indices[0]].start_time
-    chr[indices[0]].start_time = chr[indices[1]].start_time 
+    chr[indices[0]].start_time = chr[indices[1]].start_time
     chr[indices[1]].start_time = temp_start_time
 
     temp_start_day = chr[indices[0]].start_day
     chr[indices[0]].start_day = chr[indices[1]].start_day
     chr[indices[1]].start_day = temp_start_day
-    
+
     return chr
 
 
@@ -33,8 +34,9 @@ def change_timeslot(chr):
     indice = rd.randint(0, len(chr))
 
     # Change the value of "Start_Time" and "Start_day " of a random gene
-    chr[indice].start_time = rd.randrange((tb.END_TIME - tb.START_TIME + 1) // 30 + 1) * tb.MINUTES_PER_CELL + tb.START_TIME
-    chr[indice].start_day = rd.randint(0,tb.DAYS)
+    chr[indice].start_time = rd.randrange(
+        (tb.END_TIME - tb.START_TIME + 1) // 30 + 1) * tb.MINUTES_PER_CELL + tb.START_TIME
+    chr[indice].start_day = rd.randint(0, tb.DAYS)
 
     return chr
 
@@ -84,7 +86,6 @@ def random_mutation(chr):
     mutation_aleatoire = rd.choice(mutations)
     return mutation_aleatoire(chr)
 
-    
 
 if __name__ == '__main__':
     print("Mutation...")
@@ -101,4 +102,3 @@ if __name__ == '__main__':
     """
     print("Random_mutation")
     print(random_mutation(CHROMOSOME_1))
-    
