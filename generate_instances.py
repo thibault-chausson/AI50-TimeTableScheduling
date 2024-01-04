6a700f0a149745b2d3643aed9bad518d24f692ee
thibault-chausson <chausson.thibault@laposte.net>
2024-01-04 10:36:59 +0100

    :construction: amélioration des exports

--- generate_instances.py
+++ generate_instances.py
@@ -1,10 +1,15 @@
 from toolbox_student import *
-from toolbox import *
 from initial_population import *
+import toolbox as tb
+import os
+import genetique as gen
+import json
 
-def get_instance(sites, formations, pop_size, promo_size, export=False):
+
+def get_instance(sites, formations, pop_size, promo_size, export_path='./datas/instances/coucou', export=False):
     """
     Generates the population and the promo from the given sites and formations
+    :param export_path:
     :param sites: list of sites
     :param formations: list of formations
     :param pop_size: size of the population
@@ -20,15 +25,37 @@ def get_instance(sites, formations, pop_size, promo_size, export=False):
         uvs.extend(get_uvs(formation))
     uvs = [i for i in uvs if i.teachers != [None]]
 
+    list_uv = []
+    for uv in uvs:
+        list_uv.append(uv.code)
+
     population = generate_population(pop_size, uvs, rooms)
-    promo = create_promo(uvs, promo_size)
+    # Sort the population and the fitness
+    fitness_sorted, population = gen.sort_dataset(population)
+
+    promo = create_promo(list_uv, promo_size)
+
+    # Correct the population
+    room_list = tb.get_rooms()
+    population, room_capacity_dict, capacity_uv_promo_dict = gen.correction_all(promo, room_list, population)
 
     if export:
-        export_population(population, "./datas/population.json")
-        export_promo(promo, "./datas/promo.json")
-    else:
-        return population, promo
+        if not os.path.exists(export_path):
+            os.makedirs(export_path)
+        export_population(population, export_path + "/population.json")
+        export_promo(promo, export_path + "/promo.json")
+        # Save the capacity of the rooms
+        with open(export_path + '/capacity_of_the_rooms.json', 'w') as file:
+            json.dump(room_capacity_dict, file)
+
+        # Save the capacity of the uvs
+        with open(export_path + '/capacity_of_the_uvs.json', 'w') as file:
+            json.dump(capacity_uv_promo_dict, file)
+
+    return population, promo, room_capacity_dict, capacity_uv_promo_dict
 
 
 if __name__ == '__main__':
-    pop, promo = get_instance(["Belfort", "Sevenans"], ["FISE-INFO", "FISA-INFO"], 10, 20, export=False)
+    pop, promo, room_capa, uv_capa = get_instance(["Belfort", "Sevenans"], ["FISE-INFO", "FISA-INFO"], 10, 20,
+                                                  export_path='./datas/instances/coucou',
+                                                  export=True)
